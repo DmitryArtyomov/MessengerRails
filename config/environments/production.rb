a7d874968b66cff4a96e782a0b44760ff312990c
Dmitry Artyomov <artyomovd@yandex.ru>
2017-03-10 00:24:44 +0300

    Add mailer settings for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,6 +56,19 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "Messenger_#{Rails.env}"
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'protected-refuge-75892.herokuapp.com'}
+  config.action_mailer.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => "gmail.com",
+    :user_name            => ENV['GMAIL_NAME'],
+    :password             => ENV['GMAIL_PASS'],
+    :authentication       => "plain",
+    :ssl                  => false,
+    :tsl                  => true,
+    :enable_starttls_auto => true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
